c73855bfba42a24cd2e336746b685e709a6cb4e4
Lenayah <lfanesie@gmail.com>
2023-09-20 13:27:04 -0400

    update

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 var lightOn = true
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lightButton: UIButton!
     @IBAction func buttonPressed(_ sender: Any) {
         lightOn.toggle()
         if lightOn {
             view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
             }
             else {
             view.backgroundColor = .black
+                lightButton.setTitle("On", for: .normal)
+
             }
 
     }
-
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+
+   
 
 }
 
