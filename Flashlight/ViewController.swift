5cb1d814e0b8ef284523ae7dc68d85530497ec44
Lenayah <lfanesie@gmail.com>
2023-09-20 13:17:17 -0400

    update

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
+var lightOn = true
+
 class ViewController: UIViewController {
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        if lightOn {
+            view.backgroundColor = .white
+            }
+            else {
+            view.backgroundColor = .black
+            }
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
